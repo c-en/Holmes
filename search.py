b0ee32909df8ae51b78665a9497c99e7539fbb38
CEN <chris.en33@gmail.com>
2018-01-06 16:32:51 -0500

    complete restructure

--- search.py
+++ search.py
@@ -12,7 +12,7 @@ class Timeout(Exception):
 def handler(sig, frame):
     raise Timeout
 
-keys = 8
+keys = 5
 
 with open('API_KEYS.csv','r') as f:
     for i, line in enumerate(f):
@@ -54,86 +54,87 @@ def crawl_url(url):
     except Timeout:
         print('took too long', url)
 
-def count_hits(text):
+
+def process(text):
+    print "***********************************************"
     query = text['question']
+    url = ('https://www.googleapis.com/customsearch/v1?key='
+        + api_key + '&cx=' + engine_id + '&q=')
+    # checking for question type
+    neg = 'NOT' in query
+    #if not ('\"' in query):
+    neg = neg or ('never' in query)
+    #    keywords = text['keywords'].replace('never', '').replace('NOT', '')
+    #    url += keywords
+    #else:
+    #    url += query
+
+    # snippet_hits counts hits in the titles and snippets of the google search
+    snippet_results = snippet_hits(text,neg)
+
+    # check if snippets are too close to call
+    if neg:
+        func = min
+    else:
+        func = max
+    sn_hits = list(snippet_results['sn_hits'])
+    best_sns = func(sn_hits)
+    sn_hits.remove(best_sns)
+    if not (abs(best_sns - func(sn_hits)) <= 1.5):
+        print "NO PAGE HITS"
+        pg_hits = [0,0,0]
+    else:
+        pg_hits = page_hits(text,snippet_results['results'])
+
+    # combined hit score, with snippets 1/3 weight and pages 2/3 weight
+    # aka words appearing in snippets counted 50% more
+    a_sns = normalize(snippet_results['sn_hits'])
+    a_pgs = normalize(pg_hits)
+    a_tots = [sn+2.*pg for sn,pg in zip(a_sns,a_pgs)]
+    print "TOTAL SCORE: " + str(a_tots)
+    print "NEGATIVE: " + str(neg)
+    return text[best_answer(a_tots,neg)]
+
+def snippet_hits(text, neg):
+    query = text['keywords']
     
     a1 = text['a1'].upper()
     a2 = text['a2'].upper()
     a3 = text['a3'].upper()
 
     url = ('https://www.googleapis.com/customsearch/v1?key='
-        + api_key + '&cx=' + engine_id + '&q=')
+        + api_key + '&cx=' + engine_id + '&q=' + query)
+    print url
 
     # hits in snippets
-    a1_sn = 0
-    a2_sn = 0
-    a3_sn = 0
-
-    # checking for question type
-    neg = 'NOT' in query
-    if not ('\"' in query):
-        neg = neg or ('never' in query)
-        keywords = text['keywords'].replace('never', '').replace('NOT', '')
-        url += keywords
-    else:
-        url += query
-
-    if neg:
-        print "negative!"
+    sn_hits = [0,0,0]
 
     # count hits in snippets, weighted by 1/(result num)
     r = requests.get(url)
     for i, hit in enumerate(r.json()['items']):
         snippet = hit['snippet'].upper() + ' ' + hit['title'].upper()
-        a1_sn += float(snippet.count(a1))*(1./float(i+1))
-        a2_sn += float(snippet.count(a2))*(1./float(i+1))
-        a3_sn += float(snippet.count(a3))*(1./float(i+1))
+        sn_hits[0] += float(snippet.count(a1))*(1./float(i+1))
+        sn_hits[1] += float(snippet.count(a2))*(1./float(i+1))
+        sn_hits[2] += float(snippet.count(a3))*(1./float(i+1))
 
-    # check if snippets are different enough to stop,
-    # or if too close to call
-    sns = [a1_sn,a2_sn,a3_sn]
-    print(sns)
-    if neg:
-        func = min
-    else:
-        func = max
-    best_sns = func(sns)
-    sns.remove(best_sns)
-    print [a1_sn,a2_sn,a3_sn]
-    if not (abs(best_sns - func(sns)) <= 1.5):
-        return text[best_answer([a1_sn,a2_sn,a3_sn], neg)]
+    print "SNIPPET HITS: " + str(sn_hits)
+    return {'sn_hits':sn_hits, 'results':r.json()['items']}
 
-    # hits in pages
-    a1_pg = 0
-    a2_pg = 0
-    a3_pg = 0
-
-    # count hits in pages, weighted by 1/(result num)
+def page_hits(text, results):
+    pg_hits = [0,0,0]
     for i in range(5):
         try:
-            url = r.json()['items'][i]['link']
+            url = results[i]['link']
             s = requests.get(url)
             page_upper = s.text.upper()
-            a1_pg += float(page_upper.count(a1))*(1./float(i+1))
-            a2_pg += float(page_upper.count(a2))*(1./float(i+1))
-            a3_pg += float(page_upper.count(a3))*(1./float(i+1))
+            pg_hits[0] += float(page_upper.count(text['a1'].upper()))*(1./float(i+1))
+            pg_hits[1] += float(page_upper.count(text['a2'].upper()))*(1./float(i+1))
+            pg_hits[2] += float(page_upper.count(text['a3'].upper()))*(1./float(i+1))
         except:
             pass
 
-    # combined hit score, with snippets 1/3 weight and pages 2/3 weight
-    # aka words appearing in snippets counted 50% more
-    a_sns = normalize([a1_sn,a2_sn,a3_sn])
-    a_pgs = normalize([a1_pg,a2_pg,a3_pg])
-    a_tots = [sn+2.*pg for sn,pg in zip(a_sns,a_pgs)]
-    return text[best_answer(a_tots,neg)]
-
-# def find_date(text):
-#     query1 = text['keywords'] + ' ' + text['a1']
-#     query2 = text['keywords'] + ' ' + text['a2']
-#     query3 = text['keywords'] + ' ' + text['a3']
-#     def count_dates(query):
-#         url = url = ('https://www.googleapis.com/customsearch/v1?key='
-#         + api_key + '&cx=' + engine_id + '&q='+query+'')
+    print "PAGE HITS: " + str(pg_hits)
+    return pg_hits
 
 def total_results(text):
     q1 = text['question'] + ' ' + text['a1']
@@ -150,10 +151,9 @@ def total_results(text):
     results2 = r2.json()['searchInformation']['totalResults']
     results3 = r3.json()['searchInformation']['totalResults']
 
-    print [results1,results2,results3]
+    print "TOTAL HITS: " + str([results1,results2,results3])
     return [results1,results2,results3]
     
 
 test = {'keywords': u'What U.S. town music venue allows Americans watch live, in-person concerts Canada? ', 'a1': u'Derby Line', 'a3': u'Niagara Falls', 'a2': u'Portal', 'question': u'What U.S. town has a music venue which allows Americans to watch live, in-person concerts from Canada? '}
 
-total_results(test)
